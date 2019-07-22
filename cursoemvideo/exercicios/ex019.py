4cb6063238c595edcd5398b8924bdde49a613a11
= <=>
2019-07-22 07:06:00 -0300

    ex019

---
+++ cursoemvideo/exercicios/ex019.py
@@ -0,0 +1,14 @@
+'''
+19 - Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido.
+'''
+
+from random import randrange, choice
+
+alunos = []
+
+for x in range(4):
+    alunos.append(input('Degite o nomw de um aluno: '))
+
+'''                                                 poderia ser random.choice(alunos)'''
+print('O aluno escolhido para apagar o quadro é {}'.format(alunos[randrange(4)]))
