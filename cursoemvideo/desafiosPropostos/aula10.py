dba4f369319bb1534cc44d9d0014e242a14fb84c
= <=>
2019-09-04 07:29:29 -0300

    aula 10

---
+++ cursoemvideo/desafiosPropostos/aula10.py
@@ -0,0 +1,52 @@
+"""
+ex028 - escreva um programa que faça o computador
+pensar em um número inteiro entre 0 e 5 e peça para o usuário
+terntar descobrir qual foi o número escolhido pelo computador.
+
+o progrma deverá escrever na tela se o usuário venceu ou perdeu.
+"""
+
+"""
+ex029 - Escreva um programa que leia o velocímetro 
+de um carro.
+Se els ultrapassar 80Km/h, mostre uma mensagem dizendo 
+que ele foi multado.
+
+A multa vai custar R$57,00 por cada Km acima do limite.
+"""
+
+"""
+ex030 - Crie um programa que leia um número inteiro
+e  mostre na tela se ele é par ou ímpar
+"""
+
+"""
+ex031 - Desenvolva um programa que pergunte a distância 
+de uma viagem em Km.
+Calcula o preço da passagem, cobrando R$o,50 por Km para 
+viagens de até 200Km e R$0,45 para viagens mais longes.
+"""
+
+"""
+ex032 - Faça um programa que leia um ano qualquer e 
+mostre se ele é bisexto.
+"""
+
+"""
+ex033 - Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+"""
+
+"""
+ex034 - Escreva um programa que pergunte o salário de um funcionário e calcule o nalor de 
+seu aumentos.
+Para salários superiores a R$1250,00, calcule um 
+aumento de 10%.
+Para os inferiores ou iguais, o aumento é de 15%.
+"""
+
+"""
+
+ex035 - Desenvolva um programa que leia o comprimento
+de três retas e diga ao usuáeio se elas podem ou não 
+formar um triângulo.
+"""
